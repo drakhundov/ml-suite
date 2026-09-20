707c23c6d684f98869948f4701a5563a71b8c76d
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-09-20 15:23:50 +0300

    OvARegressionSolver.py     Convert all matrices to a single dtype.     Compute predictions with a single function called via solver instead of relying on separate identical functions (which turned out not to be identical).     Add an HTML progress bar.

--- src/mlsuite/clf/OvALogisticClassification.py
+++ src/mlsuite/clf/OvALogisticClassification.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 
 from mlsuite.protocol import FloatArrayT
@@ -6,30 +8,24 @@ from .OvALogisticRegressionSolver import OvALogisticRegressionSolver
 
 
 class OvALogisticClassification:
-    def __init__(
-        self,
-        conf: LogisticClfConfig
-    ):
+    def __init__(self, conf: LogisticClfConfig):
         # Model hyperparameters.
         self.hp = conf
         self.solver = OvALogisticRegressionSolver(conf)
 
-    def fit(self, X_train: FloatArrayT, y_train: FloatArrayT):
+    def fit(self, X_train: FloatArrayT, y_train: FloatArrayT, dtype: Optional[np.dtype] = None):
         self.X_train = X_train
         self.y_train = y_train
 
         # Train a separate binary classification model for each class.
         # Put them all into a single matrix for efficiency.
-        self.W, self.B = self.solver.fit(X_train, y_train)
+        self.W, self.B = self.solver.fit(X_train, y_train, dtype)
 
     def predict(self, X_new: FloatArrayT) -> FloatArrayT:
-        H = self._calc_matrix_sigmoid(self.W.dot(X_new) + self.B)
-        return H
-
-    def _calc_matrix_sigmoid(self, Z: FloatArrayT) -> FloatArrayT:
-        """Returns an array of the same shape as Z, where each element is the sigmoid of the corresponding element in Z."""
-        sigmoid_val = 1 / (1 + np.exp(-Z))
-        return sigmoid_val
+        # X_new -> (N, D)
+        #   N — number of data points
+        #   D — number of features
+        self.solver._compute_hypothesis(X_new)
 
     def get_weights_for_class(self, classno: int) -> FloatArrayT:
         w = self._W[classno, :]
--- src/mlsuite/clf/OvALogisticRegressionSolver.py
+++ src/mlsuite/clf/OvALogisticRegressionSolver.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 import numpy as np
+from IPython.display import HTML, display
 
 from mlsuite.protocol import FloatArrayT
 from .Conf import LogisticClfConfig
@@ -8,7 +11,7 @@ class OvALogisticRegressionSolver:
     def __init__(self, conf: LogisticClfConfig):
         self.hp = conf
 
-    def fit(self, X_train: FloatArrayT, y_train: FloatArrayT):
+    def fit(self, X_train: FloatArrayT, y_train: FloatArrayT, dtype: Optional[np.dtype] = None):
         # ! Assumes class numbers start from 0 and grow discretely.
         if np.max(y_train) >= self.hp.num_classes or np.min(y_train) < 0:
             raise ValueError(
@@ -24,47 +27,75 @@ class OvALogisticRegressionSolver:
         # apply each to data point and use.
         N, D = X_train.shape
         if N != y_train.size:
-            raise ValueError(f"X_train(shape {X_train.shape}) and y_train(shape {y_train.shape}) don't match")
+            raise ValueError(
+                f"X_train(shape {X_train.shape}) and y_train(shape {y_train.shape}) don't match"
+            )
+        
+        if dtype is None:
+            dtype = X_train.dtype
+        else:
+            X_train = X_train.astype(dtype)
 
         # Train a separate binary classification model for each class.
         # Put them all into a single matrix for efficiency.
-        W = np.random.randn(D, self.hp.num_classes) * 0.01
+        self.W = np.random.randn(D, self.hp.num_classes).astype(dtype) * 0.01
         if self.hp.use_bias:
-            B = np.zeros((1, self.hp.num_classes))
+            self.B = np.zeros((1, self.hp.num_classes)).astype(dtype)
         else:
-            B = 0
-
-        def _compute_hypothesis():
-            nonlocal X_train, W, B
-            """
-            Computes the value of the hypothesis according to the logistic regression rule.
-            Y = sigmoid(W*X + b)
-            """
-            Z = X_train @ W + B
-            return self._calc_matrix_sigmoid(Z)
+            self.B = 0
 
         Y = np.zeros((N, self.hp.num_classes))
         Y[np.arange(N), y_train] = 1
 
         lr = self.hp.lr
+        progress_bar = display(
+            HTML(
+                '<progress value="0" max="{}"></progress> 0%'.format(
+                    self.hp.niters
+                )
+            ),
+            display_id=True,
+        )
         for _ in range(self.hp.niters):
             # Compute hypothesis.
-            A = _compute_hypothesis()
+            A = self._compute_hypothesis(X_train)
             # Calculate error.
             pure_error = A - Y  # (N, C)
             # For Cross-Entropy, sigmoid function cancels its own derivative.
             # Update the weights and biases accordingly.
-            W = W - lr * (
+            self.W = self.W - lr * (
                 (1 / N) * X_train.T @ pure_error  # Gradient (D, C)
-                + (self.hp.l2_coef * W)  # L2 Regularization
+                + (self.hp.l2_coef * self.W)  # L2 Regularization
             )
             if self.hp.use_bias:
-                B = B - lr * (1 / N) * np.sum(pure_error, axis=0, keepdims=True)
+                self.B = self.B - lr * (1 / N) * np.sum(
+                    pure_error, axis=0, keepdims=True
+                )
             if self.hp.diminishing_lr:
                 lr *= self.hp.lr_dim_coef
-        return W, B
+            completed = _ + 1
+            progress_bar.update(
+                HTML(
+                    '<progress value="{}" max="{}"></progress> {}%'.format(
+                        completed,
+                        self.hp.niters,
+                        int(completed / self.hp.niters * 100),
+                    )
+                )
+            )
+        return self.W, self.B
 
     def _calc_matrix_sigmoid(self, Z: FloatArrayT) -> FloatArrayT:
         """Returns an array of the same shape as Z, where each element is the sigmoid of the corresponding element in Z."""
         sigmoid_val = 1 / (1 + np.exp(-Z))
         return sigmoid_val
+
+    def _compute_hypothesis(self, X: FloatArrayT):
+        if self.B is None:
+            self.B = 0
+        """
+        Computes the value of the hypothesis according to the logistic regression rule.
+        Y = sigmoid(W*X + b)
+        """
+        Z = X @ self.W + self.B
+        return self._calc_matrix_sigmoid(Z)
